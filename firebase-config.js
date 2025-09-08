dabd34c17358b13ce61799d402aa34bf438fb275
Faamil007 <mohamedfaamil@gmail.com>
2025-09-09 00:39:24 +0400

    Initial commit

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+        // Firebase configuration - replace with your actual config
+        const firebaseConfig = {
+            apiKey: "AIzaSyAEvywD1zXdcsvvfJpULp3rrWVo51Okc_w",
+            authDomain: "rifa-a3547.firebaseapp.com",
+            projectId: "rifa-a3547",
+            storageBucket: "rifa-a3547.firebasestorage.app",
+            messagingSenderId: "69916935947",
+            appId: "1:69916935947:web:46c9d34895d7c5b889840e"
+          };
+                  
+                  // Initialize Firebase
+                  firebase.initializeApp(firebaseConfig);
